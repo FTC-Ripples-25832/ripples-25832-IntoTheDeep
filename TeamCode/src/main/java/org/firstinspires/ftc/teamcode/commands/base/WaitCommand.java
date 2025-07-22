e79c4f61d5314ca9d4822a851a1eaf0d58c08a65
137 <113233555+caezium@users.noreply.github.com>
2025-07-22 19:45:24 +0800

    revert to old wait command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/base/WaitCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/base/WaitCommand.java
@@ -1,14 +1,15 @@
 package org.firstinspires.ftc.teamcode.commands.base;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.subsystems.base.SubsystemBase;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class WaitCommand implements Command {
         private final double seconds;
-        private final ElapsedTime timer = new ElapsedTime();
+        private long startTime;
 
         public WaitCommand(double seconds) {
                 this.seconds = seconds;
@@ -16,7 +17,7 @@ public class WaitCommand implements Command {
 
         @Override
         public void initialize() {
-                timer.reset();
+                startTime = System.currentTimeMillis();
         }
 
         @Override
@@ -26,7 +27,7 @@ public class WaitCommand implements Command {
 
         @Override
         public boolean isFinished() {
-                return timer.seconds() >= seconds;
+                return System.currentTimeMillis() - startTime >= seconds * 1000;
         }
 
         @Override
